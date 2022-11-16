1d9844cb4259860c4ab7da5923479d11dc26a32e
sstcheuffa <stcheuffa@alumni.harvard.edu>
2022-11-16 14:49:24 -0500

    Disabled classilink and implemented private_subnets resource.

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -6,7 +6,7 @@ resource "aws_vpc" "application_vpc" {
   instance_tenancy     = "default"
   enable_dns_support   = "true"
   enable_dns_hostnames = "true"
-  enable_classiclink   = "false"
+  # enable_classiclink   = "false"
 
   tags = {
     Name = "${var.environment}_application_vpc"
@@ -28,6 +28,16 @@ resource "aws_subnet" "public_subnets" {
 
 resource "aws_subnet" "private_subnets" {
   # Implement this resource
+    vpc_id                  = aws_vpc.application_vpc.id
+  cidr_block              = element(var.private_subnet_cidr_blocks, count.index)
+  availability_zone       = element(var.availability_zones, count.index)
+  map_public_ip_on_launch = false
+  
+  count                   = length(var.private_subnet_cidr_blocks)
+
+  tags = {
+    Name = "${var.environment}_private_subnet_${substr(element(var.availability_zones, count.index), -1, 1)}"
+  }
 }
 
 # Internet Gateway
